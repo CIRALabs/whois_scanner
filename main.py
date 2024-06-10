a3639e4ed9aa86cc0fcdf3b82273ae262a215a83
Bill Belanger <bill.belanger@cira.ca>
2024-06-10 12:08:31 -0400

    Nicer error handling

--- main.py
+++ main.py
@@ -17,8 +17,14 @@ def read_input():
     raise WhoisCrawlerException(ErrorCodes.FAILED_TO_READ_INPUT_FILE, ex)
 
 def lookup(domain):
-  resp = whois.whois(domain)
-  return resp
+  try:
+    resp = whois.whois(domain)
+    return resp
+  except whois.parser.PywhoisError as ex:
+    if str(ex).startswith("No match for"):
+      raise WhoisCrawlerException(ErrorCodes.HOSTNAME_DOES_NOT_EXIST)
+    else:
+      raise ex
 
 def extract_registrant_country(whois_result):
   if "country" in whois_result:
@@ -43,18 +49,36 @@ def main():
   try:
     data = read_input()
     domains = extract_domains(data)
-    for domain in domains:
-      hostname = extract_hostname(domain)
-      whois_result = lookup(hostname)
-      country = extract_registrant_country(whois_result)
-      DB.record_country(hostname, country)
-    DB.print_results()
   except WhoisCrawlerException as whoisexception:
     log.exception(whoisexception)
     return whoisexception.code
   except BaseException as ex:
     log.exception(ex)
     return -1
+  
+  failed_domains = []
+  for domain in domains:
+    try:
+      hostname = extract_hostname(domain)
+      whois_result = lookup(hostname)
+      country = extract_registrant_country(whois_result)
+      DB.record_country(hostname, country)
+    except WhoisCrawlerException as whoisexception:
+      failed_domains.append({"domain": domain, "cause": str(whoisexception)})
+      # Continue processing, but record the failure
+    except whois.parser.PywhoisError as ex:
+      failed_domains.append({"domain": domain, "cause": ex})
+      # Continue processing, but record the failure
+    except Exception as ex:
+      log.exception(ex)
+      return -1 # stop processing immediately
+  DB.print_results()
+  if len(failed_domains) > 0:
+    log.error("Failed domains:")
+    log.error(failed_domains)
+    return 100
+  else:
+    return 0
 
 if __name__ == "__main__":
   exit(main())
