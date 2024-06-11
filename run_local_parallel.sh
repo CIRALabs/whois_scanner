1fe1b2eb360944fae3139c4114f3b24a25091a39
Bill Belanger <bill.belanger@cira.ca>
2024-06-11 15:48:49 -0400

    Modify parallel script to output to unique files per thread

--- run_local_parallel.sh
+++ run_local_parallel.sh
@@ -14,7 +14,7 @@ threads=$pages
 
 # Note: Can't use the constant variable at the top for file name because the command runs in a separate bash shell
 rm $EXIT_CODES_FILE 2> /dev/null
-seq 0 $(($pages - 1)) | xargs -n 1 -P $threads bash -c 'python main.py $2 $1; echo $? >> /tmp/whois_scanner_exit_codes.tmp' -- $pagesize
+seq 0 $(($pages - 1)) | xargs -n 1 -P $threads bash -c 'python main.py $2 $1 > /tmp/whois_scanner_results_$2.json; echo $? >> /tmp/whois_scanner_exit_codes.tmp' -- $pagesize
 
 echo
 failed_count=0
